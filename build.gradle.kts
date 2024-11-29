a6389d545450755f5686f652459820211ff6b614
Stephen <138625055+stephendotgg@users.noreply.github.com>
2024-11-29 22:00:24 +0000

    placeholders

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "org.mclicense"
-version = "1.3.1"
+version = "1.3.2"
 
 repositories {
     maven("https://repo.papermc.io/repository/maven-public/")
--- src/main/java/org/mclicense/library/MarketplaceProvider.java
+++ src/main/java/org/mclicense/library/MarketplaceProvider.java
@@ -3,7 +3,7 @@ package org.mclicense.library;
 // Supported marketplaces: Polymart
 public class MarketplaceProvider {
     private static String pmPlaceholder = "%%__POLYMART__%%";
-    private static String pmLicense = "%%__LICENSE__%%";
+    private static String pmLicense = "%%__VERIFY_TOKEN__%%";
 
     protected static String getHardcodedLicense() {
         if (pmPlaceholder.equals("1") && !pmLicense.startsWith("%%__")) {
